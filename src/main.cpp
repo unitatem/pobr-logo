eae21f8151237f3034cdef92ae44091c7f298e9c
Mariusz Wachowicz <wachowiczmariusz@gmail.com>
2018-05-24 19:42:46 +0200

    Implement very simple threshold for object detection

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,42 @@ void show_image(const cv::Mat &image) {
     cv::waitKey(-1);
 }
 
+bool op_ge(const cv::Vec3b &value, const cv::Vec3b &ref) {
+    return value[0] >= ref[0] && value[1] >= ref[1] && value[2] >= ref[2];
+}
+
+bool op_le(const cv::Vec3b &value, const cv::Vec3b &ref) {
+    return value[0] <= ref[0] && value[1] <= ref[1] && value[2] <= ref[2];
+}
+
+cv::Mat segment_threshold_to_rgb(const cv::Mat &image, const cv::Vec3b &low, const cv::Vec3b &high) {
+    assert(image.channels() == 3);
+    cv::Mat result(image.rows, image.cols, CV_8UC3, cv::Vec3b(0, 0, 0));
+
+    for (auto r = 0; r < image.rows; ++r)
+        for (auto c = 0; c < image.cols; ++c) {
+            const auto &pixel = image.at<cv::Vec3b>(r, c);
+            if (op_ge(pixel, low) && op_le(pixel, high))
+                result.at<cv::Vec3b>(r, c) = pixel;
+        }
+
+    return result;
+}
+
+cv::Mat segment_threshold_to_mono(const cv::Mat &image, const cv::Vec3b &low, const cv::Vec3b &high) {
+    assert(image.channels() == 3);
+    cv::Mat result(image.rows, image.cols, CV_8UC1, cv::Scalar(0));
+
+    for (auto r = 0; r < image.rows; ++r)
+        for (auto c = 0; c < image.cols; ++c) {
+            const auto &pixel = image.at<cv::Vec3b>(r, c);
+            if (op_ge(pixel, low) && op_le(pixel, high))
+                result.at<uchar >(r, c) = 255;
+        }
+
+    return result;
+}
+
 int main() {
     std::cout << "START" << std::endl;
 
@@ -28,8 +64,15 @@ int main() {
     auto img_path = std::experimental::filesystem::directory_iterator(images_path)->path().string();
     DEBUG(std::cout << "img_path = " << img_path << std::endl;)
 
-    auto image = load_image(img_path, cv::ImreadModes::IMREAD_COLOR);
-    show_image(image);
+    auto img = load_image(img_path, cv::ImreadModes::IMREAD_COLOR);
+    show_image(img);
+
+    // bgr
+    auto img_white = segment_threshold_to_mono(img, cv::Vec3b(230, 230, 230), cv::Vec3b(250, 250, 240));
+    show_image(img_white);
+
+    auto img_yellow = segment_threshold_to_mono(img, cv::Vec3b(0, 200, 240), cv::Vec3b(100, 230, 255));
+    DEBUG(show_image(img_yellow);)
 
     return 0;
 }
